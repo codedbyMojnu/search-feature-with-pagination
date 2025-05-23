2dc209017f8d6787b2380c92f12c89e84143b051
codebymojnu <thisismojnu@gmail.com>
2025-05-23 19:52:56 +0600

    active page add logic

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -15,7 +15,11 @@ export default function Pagination({ onPageValueChange, pageNumbers }) {
             handleActivePage(number);
           }}
           key={index}
-          className="mx-1 px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className={`mx-1 px-4 py-2 text-sm font-medium  border border-gray-300 rounded hover:bg-gray-100 ${
+            acitvePage === number
+              ? "bg-blue-500 text-white"
+              : "bg-white text-gray-700 "
+          }`}
         >
           {number}
         </button>
--- src/components/UsersInfo.jsx
+++ src/components/UsersInfo.jsx
@@ -4,19 +4,21 @@ export default function Data({ searchQuery, usersData }) {
     <div className="flex flex-col sm:flex-row justify-center items-center gap-6">
       {searchQuery.trim() !== ""
         ? usersData?.map((user) => (
-            <div
-              key={user.id}
-              className="bg-white shadow-md rounded-xl p-4 flex flex-col items-center space-y-3 hover:shadow-lg transition-shadow duration-300"
-            >
-              <img
-                src={user.avatar_url}
-                alt={user.login}
-                className="h-32 w-32 rounded-full object-cover border-4 border-blue-100"
-              />
-              <p className="text-lg font-semibold text-gray-800">
-                {user.login}
-              </p>
-            </div>
+            <a href={user.html_url} className="cursor-pointer">
+              <div
+                key={user.id}
+                className="bg-white shadow-md rounded-xl p-4 flex flex-col items-center space-y-3 hover:shadow-lg transition-shadow duration-300"
+              >
+                <img
+                  src={user.avatar_url}
+                  alt={user.login}
+                  className="h-32 w-32 rounded-full object-cover border-4 border-blue-100"
+                />
+                <p className="text-lg font-semibold text-gray-800">
+                  {user.login}
+                </p>
+              </div>
+            </a>
           ))
         : "Try to Search Your Github Name"}
     </div>
